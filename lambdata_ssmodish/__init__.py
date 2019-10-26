33779e78444380284aefc367b9c6bbb1c5dec44d
Steve Smodish <smodish@hotmail.com>
2019-10-25 23:21:55 -0400

    added some proof of concept utility functions

---
+++ lambdata_ssmodish/__init__.py
@@ -0,0 +1,13 @@
+"""
+lambdata - a collection of data science helper functions
+"""
+
+import pandas as pd
+import numpy as np
+
+#sample code
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.zeros(50))
+
+
+
